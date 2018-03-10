ad8a4011d385df34643627d648ae0868b320c3ce
SnivyDroid <33987596+Pygasm@users.noreply.github.com>
2018-03-10 15:28:18 -0700

    Added tones, programmed generation system

--- TicTacToe/Classes.h
+++ TicTacToe/Classes.h
@@ -3,17 +3,20 @@
 
 #pragma once
 #include <Arduboy2.h>
+#include <ArduboyTones.h>
 
 // Board
 class Board
 {
   public:
-  	void upd(Arduboy2 & arduboy, int cells[3][9]);
+  	void upd(Arduboy2 & arduboy, int cells[3][9][2]);
   
   private:
   	const int pos[2] = {43, 9};
   	const int len[2] = {46, 46};
   	static const unsigned char image[];
+    static const unsigned char x[];
+    static const unsigned char o[];
 };
 
 
@@ -21,7 +24,7 @@ class Board
 class Selector
 {
   public:
-    void upd(Arduboy2 & arduboy);
+    void upd(Arduboy2 & arduboy, ArduboyTones & sound);
     int posX[3] = {43, 59, 75};
     int posY[3] = {9, 25, 41};
     int len[2] = {15, 15};
@@ -32,3 +35,4 @@ class Selector
   private:
     static const unsigned char image[];
 };
+
